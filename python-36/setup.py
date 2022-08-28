6d5e08ca53ba5925ee01a679f551fde29f446423
maxusmusti <meyceoz@redhat.com>
2022-08-28 03:18:08 -0400

    1.0.0 Release

---
+++ python-36/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from pathlib import Path
+
+setup(name='python36-state_signals',
+    version='1.0.0',
+    description='Package for easy management of state/event signal publishing, subscribing, and responding',
+    url='https://github.com/distributed-system-analysis/state-signals/',
+    author='Mustafa Eyceoz',
+    author_email='meyceoz@redhat.com',
+    license='GPL3',
+    py_modules=['state_signals'],
+    zip_safe=False)
