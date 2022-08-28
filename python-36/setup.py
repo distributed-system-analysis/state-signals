3f3864fcadbee5a9a4518266a8145e66665bf2af
maxusmusti <meyceoz@redhat.com>
2022-08-28 03:28:25 -0400

    Reformatting

--- python-36/setup.py
+++ python-36/setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 from pathlib import Path
 
-setup(name='python36-state_signals',
-    version='1.0.0',
-    description='Package for easy management of state/event signal publishing, subscribing, and responding',
-    url='https://github.com/distributed-system-analysis/state-signals/',
-    author='Mustafa Eyceoz',
-    author_email='meyceoz@redhat.com',
-    license='GPL3',
-    py_modules=['state_signals'],
-    zip_safe=False)
+setup(
+    name="python36-state_signals",
+    version="1.0.0",
+    description="Package for easy management of state/event signal publishing, subscribing, and responding",
+    url="https://github.com/distributed-system-analysis/state-signals/",
+    author="Mustafa Eyceoz",
+    author_email="meyceoz@redhat.com",
+    license="GPL3",
+    py_modules=["state_signals"],
+    zip_safe=False,
+)
